ed522aafadc41e4cc70160323d241b37c02eda0f
talesxavier1 <Talesxavier1@gmail.com>
2024-03-02 00:42:10 -0300

    .

--- content.js
+++ content.js
@@ -1,20 +1,25 @@
 
-const main = () => {
-    let script = getScript();
+const main = async () => {
+    let gitHubPageGet = await getScript();
+    debugger;
     eval(script);
 }
 
-const getScript = () => {
-    $.ajax({
-        url: "https://raw.githubusercontent.com/talesxavier1/Json-Schema/main/jsonSchemaLib/js/JS_Main.js",
+const getScript = async () => {
+    let _response = "(()=>{})()";
+    await $.ajax({
+        url: "https://raw.githubusercontent.com/talesxavier1/ext/dev/Main.js",
         type: "GET",
+        headers: {
+            "Accept": "*/*",
+        },
         success: function (response) {
-            return response;
+            _response = response;
         },
         error: function (xhr, status, error) {
-            return "(()=>{})()";
         }
     });
+    return _response
 }
 
 main();
