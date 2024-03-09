a5f7d53865f0afd12021b266862dc0f030340c60
talesxavier1 <Talesxavier1@gmail.com>
2024-03-09 00:54:47 -0300

    a

--- Main.js
+++ Main.js
@@ -70,7 +70,7 @@ const main_componentsObserve = () => {
         for (let MUTATION of mutationsList) {
             for (let ADDED_NODES of MUTATION.addedNodes) {
                 if (ADDED_NODES.localName == "iframe") {
-                    ADDED_NODES.remove();
+                    //ADDED_NODES.remove();
                 }
             }
         }
